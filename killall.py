70e6a5185eb1ccab392e75956764c079f305f448
ledinhhuy <ledinhhuy@users.noreply.github.com>
2019-02-03 13:26:05 +0000

    Update killall.py

--- killall.py
+++ killall.py
@@ -1,8 +1,8 @@
 import psutil
 import requests
-r = requests.get("https://gist.githubusercontent.com/ledinhhuy/2acd143b6ee5bcdf87b78b23a77a2c51/raw/3571008c7d4983b3271e2cca3c37916c9d6b76d1/kill_ignore.txt")
+r = requests.get("https://pastebin.com/raw/h7KiWPtq")
 kill_ignore = set(r.content.splitlines())
-print kill_ignore
+print "ignore", kill_ignore
 for p in psutil.process_iter():
     try:
         if p.name() not in kill_ignore:
